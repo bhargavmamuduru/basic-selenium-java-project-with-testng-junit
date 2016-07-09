d37e757f6c61826ec8c5b544fe1870792c1f3950
bhargavmamuduru <bhargavmamuduru@gmail.com>
2016-07-09 13:04:14 -0700

    Added main method with chrome driver and selenium dependency in pom file

---
+++ src/test/java/com/fly9/selenium/WebDriverTest.java
@@ -0,0 +1,26 @@
+package com.fly9.selenium;
+
+//Importing Selenium packages
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * This class demonstrates that you can create a new webDriver instance object
+ * Open google.com site and close
+ * Created by Balu on 7/8/2016.
+ */
+public class WebDriverTest {
+
+    //Main method to demonstrate running Web driver.
+    public static void main(String[] args)
+    {
+        //Creating a Web Driver instance.
+        WebDriver driver = new ChromeDriver();
+
+        //Open google.com site
+        driver.get("https://www.google.com");
+
+        //Closing the browser session
+        driver.close();
+    }
+}
